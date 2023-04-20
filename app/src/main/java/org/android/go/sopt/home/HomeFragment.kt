7cc756656d3b2d17f351010a8247300bc81a8a60
jooyyoo <aliceyun9@naver.com>
2023-04-20 20:11:08 +0900

    feature: ViewModel 구현 추가

--- app/src/main/java/org/android/go/sopt/home/HomeFragment.kt
+++ app/src/main/java/org/android/go/sopt/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.ConcatAdapter
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentHomeBinding
@@ -17,30 +18,7 @@ class HomeFragment : Fragment() {
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "이렇게 쓰면 안됩니다~" }
 
-    private val mockProfileList = listOf<Profile>(
-        Profile(
-            image = R.drawable.snoopy, name = "snoopy1", age = "2"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy1", age = "1"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy2", age = "3"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy4", age = "4"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy5", age = "5"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy6", age = "6"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy7", age = "7"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy8", age = "8"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy9", age = "9"
-        ), Profile(
-            image = R.drawable.snoopy, name = "snoopy10", age = "10"
-        )
-    )
-
+    private val viewModel by viewModels<HomeViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -50,13 +28,13 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //대부분의 로직은 여기에 구현된다.
+
         val profileAdapter = ProfileAdapter(requireContext())
         val titleAdapter = TitleAdapter(requireContext())
 
         val concatAdapter = ConcatAdapter(titleAdapter, profileAdapter)
         binding.recyclerview.adapter = concatAdapter
-        profileAdapter.setProfileList(mockProfileList)
+        profileAdapter.setProfileList(viewModel.mockProfileList)
     }
 
     override fun onDestroyView() {
