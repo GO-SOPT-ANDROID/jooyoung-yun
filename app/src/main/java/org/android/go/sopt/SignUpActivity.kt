fa132194073ab8ecdac08c91bf473cb3323276f4
jooyyoo <aliceyun9@naver.com>
2023-04-21 23:00:13 +0900

    feature: 심화과제 완료

--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -1,6 +1,10 @@
 package org.android.go.sopt
 
+import android.content.Context
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Input
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.databinding.ActivitySignupBinding
@@ -11,6 +15,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
+
+        binding.root.setOnClickListener{
+            hideKeyboard()
+        }
+
         setContentView(binding.root)
 
         binding.btSignUpBtn.setOnClickListener {
@@ -22,7 +31,6 @@ class SignUpActivity : AppCompatActivity() {
     fun signIn() {
         if (binding.etNewID.length() in 6..10 && binding.etNewPW.length() in 8..12) {
 
-            Snackbar.make(binding.root, getString(R.string.id_need), Snackbar.LENGTH_SHORT).show()
             val id = binding.etNewID.text.toString()
             val pw = binding.etNewPW.text.toString()
             val name = binding.etName.text.toString()
@@ -36,8 +44,18 @@ class SignUpActivity : AppCompatActivity() {
 
             setResult(RESULT_OK, intent)
             finish()
-        } else {
+        } else if (binding.etNewID.length() < 6 || binding.etNewID.length() > 10) {
+            Snackbar.make(binding.root, getString(R.string.id_need), Snackbar.LENGTH_SHORT).show()
+        }else if (binding.etNewPW.length() < 8 || binding.etNewPW.length() > 12) {
             Snackbar.make(binding.root, getString(R.string.pw_need), Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    fun hideKeyboard(){
+        if(this != null){
+            val imm : InputMethodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as  InputMethodManager
+            imm.hideSoftInputFromWindow(this.currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+
     }
 }
