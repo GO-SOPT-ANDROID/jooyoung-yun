61bdb7576a8de989b4b223cacab4cd38dfd8d47c
jooyyoo <aliceyun9@naver.com>
2023-05-13 02:51:02 +0900

    feat: Api 추가

--- app/src/main/java/org/android/go/sopt/data/local/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/local/ApiFactory.kt
@@ -15,7 +15,19 @@ object ApiFactory {
     inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
 }
 
+object ReqresApiFactory {
+    val Reqretrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://reqres.in/")
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+    inline fun <reified T> create(): T = Reqretrofit.create<T>(T::class.java)
+}
+
 object ServicePool {
     val signUpService = ApiFactory.create<SignUpService>()
     val logInService = ApiFactory.create<LogInService>()
+    val followersService = ReqresApiFactory.create<FollowersService>()
+
 }
