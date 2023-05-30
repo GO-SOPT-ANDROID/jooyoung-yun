ccd2a8abe5db7ca892ac0fd9973c2ea9c97a9349
jooyyoo <aliceyun9@naver.com>
2023-05-31 01:11:58 +0900

    [chore] 주석 추가

--- app/src/main/java/org/android/go/sopt/presentation/gallery/adapter/GalleryAdapter.kt
+++ app/src/main/java/org/android/go/sopt/presentation/gallery/adapter/GalleryAdapter.kt
@@ -5,10 +5,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.android.go.sopt.databinding.ItemPagerBinding
 
-class GalleryAdapter(_itemList: List<Int> = listOf(),) :
+class GalleryAdapter(_itemList: List<Int> = listOf()) :
     RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>() {
     private var itemList: List<Int> = _itemList
 
+    /*ViewHolder 에 들어갈 view를 만들어 준다*/
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -17,13 +18,13 @@ class GalleryAdapter(_itemList: List<Int> = listOf(),) :
         return GalleryViewHolder(binding)
     }
 
-    class GalleryViewHolder(private val binding: ItemPagerBinding,) :
+    class GalleryViewHolder(private val binding: ItemPagerBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(src: Int) {
             binding.imgGallery.setImageResource(src)
         }
     }
-
+    /*각각의 ViewHolder 에 데이터를 매칭하는 함수. 즉, 리사이클러 뷰의 각각 아이템에 관한 내용*/
     override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
         holder.bind(itemList[position])
     }
@@ -32,6 +33,6 @@ class GalleryAdapter(_itemList: List<Int> = listOf(),) :
 
     fun setItemList(itemList: List<Int>) {
         this.itemList = itemList
-        notifyDataSetChanged()
+        notifyDataSetChanged()/*데이터를 바꾸고 마지막에 알려줘야함*/
     }
 }
