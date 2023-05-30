6087729e9fd0fb10cb68e3e3cf5a9cfda65521aa
jooyyoo <aliceyun9@naver.com>
2023-05-31 02:00:14 +0900

    [feat] 서버 통신을 위한 DTO 다시 설정 !! 중요: 모든 정보들이 다 있어야 함

--- app/src/main/java/org/android/go/sopt/data/response/ResponseFriendDto.kt
+++ app/src/main/java/org/android/go/sopt/data/response/ResponseFriendDto.kt
@@ -6,23 +6,32 @@ import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
 
 @Serializable
-data class ResponseFriendDto(
+data class ResponseFriendDto (
+    @SerialName("page")
     val page: Long? = null,
 
     @SerialName("per_page")
     val perPage: Long? = null,
 
+    @SerialName("total")
     val total: Long? = null,
 
     @SerialName("total_pages")
     val totalPages: Long? = null,
 
+    @SerialName("data")
     val data: List<FriendData>? = null,
+
+    @SerialName("support")
+    val support: Support? = null
 )
 
 @Serializable
-data class FriendData(
+data class FriendData (
+    @SerialName("id")
     val id: Long? = null,
+
+    @SerialName("email")
     val email: String? = null,
 
     @SerialName("first_name")
@@ -31,5 +40,15 @@ data class FriendData(
     @SerialName("last_name")
     val lastName: String? = null,
 
+    @SerialName("avatar")
     val avatar: String? = null
+)
+
+@Serializable
+data class Support (
+    @SerialName("url")
+    val url: String? = null,
+
+    @SerialName("text")
+    val text: String? = null
 )
