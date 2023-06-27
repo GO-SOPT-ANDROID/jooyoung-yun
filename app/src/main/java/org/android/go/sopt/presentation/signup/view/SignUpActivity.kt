152be21c03e92e9c3a2cab5b163630c8e42d1017
jooyyoo <aliceyun9@naver.com>
2023-06-27 17:34:44 +0900

    [feat] databinding 으로 코드 변경하여 구현

--- app/src/main/java/org/android/go/sopt/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/view/SignUpActivity.kt
@@ -12,13 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.ActivitySignupBinding
 import org.android.go.sopt.presentation.signup.viewmodel.SignUpViewModel
+import org.android.go.sopt.util.binding.BindingActivity
 import java.util.regex.Pattern
 
 
 /*회원가입 페이지*/
-class SignUpActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySignupBinding
-
+class SignUpActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_signup) {
     private val viewModel: SignUpViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
