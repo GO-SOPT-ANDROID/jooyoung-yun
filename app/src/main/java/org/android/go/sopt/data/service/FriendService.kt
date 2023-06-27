6df433b23a834b977737583778ae4b46823723bf
jooyyoo <aliceyun9@naver.com>
2023-06-27 23:45:39 +0900

    [feat] query 문으로 바꾸기

--- app/src/main/java/org/android/go/sopt/data/service/FriendService.kt
+++ app/src/main/java/org/android/go/sopt/data/service/FriendService.kt
@@ -1,12 +1,12 @@
 package org.android.go.sopt.data.service
 
 import org.android.go.sopt.data.response.ResponseFriendDto
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FriendService {
-    @GET("/api/users?2")
-    fun getFriend(
-    ): Call<ResponseFriendDto>
+    @GET("/api/users")
+    suspend fun fetchFriendList(
+        @Query("page") page: Int = 2,
+    ): ResponseFriendDto
 }
