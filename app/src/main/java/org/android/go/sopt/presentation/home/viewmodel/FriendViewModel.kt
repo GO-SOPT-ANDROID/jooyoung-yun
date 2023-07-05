7e69f6c7da3e6da9e9e06209243be1a8fc2aac9a
jooyyoo <aliceyun9@naver.com>
2023-07-05 17:28:12 +0900

    [mod] 팔로워 리스트 데이터 바인딩 및 생성자 코드 수정

--- app/src/main/java/org/android/go/sopt/presentation/home/viewmodel/FriendViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/home/viewmodel/FriendViewModel.kt
@@ -8,9 +8,10 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.android.go.sopt.data.repository.FriendRepositoryImpl
 import org.android.go.sopt.domain.model.Friend
+import org.android.go.sopt.domain.repository.FriendRepository
 import timber.log.Timber
 
-class FriendViewModel(val friendRepositoryImpl: FriendRepositoryImpl): ViewModel(){
+class FriendViewModel(val friendRepository: FriendRepository): ViewModel(){
     private var _friendList = MutableStateFlow<List<Friend>>(listOf())
     val friendList get() = _friendList.asStateFlow()
 
@@ -20,7 +21,7 @@ class FriendViewModel(val friendRepositoryImpl: FriendRepositoryImpl): ViewModel
 
     private fun fetchFriendList() {
         viewModelScope.launch {
-            friendRepositoryImpl.fetchFriend()
+            friendRepository.fetchFriend()
                 .onSuccess { data ->
                     _friendList.value = data
                 }
